60c95ed69405fccf77b904c6edf281350f68d782
ntnshrav <ntnshrav@gmail.com>
2017-05-30 18:13:03 +0530

    added purity,gamma statistics,jaccard co-efficient

--- SRC/validation.py
+++ SRC/validation.py
@@ -1,6 +1,7 @@
 from math import sqrt
 import numpy as np
-
+import itertools 
+from math import sqrt
 #TODO: can do one more numpy array casting inside all function
 
 def euclidean_distance(vector1,vector2,squared=False):
@@ -143,6 +144,80 @@ def Compute_fowlkes_mallows_score(labels_true, labels_pred):
 	http://scikit-learn.org/stable/modules/generated/sklearn.metrics.fowlkes_mallows_score.html#sklearn.metrics.fowlkes_mallows_score
 	"""	
 	return metrics.fowlkes_mallows_score(labels_true, labels_pred) 
+
+def Compute_purity(labels_true,labels_pred):
+	"""Purity 
+	Reference:
+	http://www.caner.io/purity-in-python.html
+	"""	
+	A = np.c_[(labels_pred,labels_true)]
+	n_accurate = 0.
+    for j in np.unique(A[:,0]):
+		z = A[A[:,0] == j, 1]
+		x = np.argmax(np.bincount(z))
+		n_accurate += len(z[z == x])
+
+    return n_accurate / A.shape[0]
+
+def Compute_jaccard_co_eff(labels_true, labels_pred):
+    """jaccard co-eff
+    chapter 10 -bible of clustering
+
+	Case a: x i and x j belong to the same clusters of C and the same category 	  of P .
+	Case b: x i and x j belong to the same clusters of C but different categories of P .
+	Case c: x i and x j belong to different clusters of C but the same category   of P .
+    j=a/(a+b+c)
+    """
+    a = b= c = 0
+    n = len(labels_true)
+    for i, j in itertools.combinations(xrange(n), 2):
+        comembership1 = labels_true[i] == labels_true[j]
+        comembership2 = labels_pred[i] == labels_pred[j]
+        if comembership1 and comembership2:
+            a += 1
+        elif comembership1 and not comembership2:
+            b += 1
+        elif not comembership1 and comembership2:
+            c += 1
+    return float(a) / (a+b+c)
+
+
+def Compute_gamma_statistics(labels_true, labels_pred):
+    """gamma statistics
+    chapter 10 -bible of clustering
+
+	Case a: x i and x j belong to the same clusters of C and the same category 	  of P .
+	Case b: x i and x j belong to the same clusters of C but different categories of P .
+	Case c: x i and x j belong to different clusters of C but the same category   of P .
+    """
+    a = b= c = d = 0
+
+	n = len(labels_true)
+	for i, j in itertools.combinations(xrange(n), 2):
+		comembership1 = labels_true[i] == labels_true[j]
+		comembership2 = labels_pred[i] == labels_pred[j]
+		if comembership1 and comembership2:
+			a += 1
+		elif comembership1 and not comembership2:
+			b += 1
+		elif not comembership1 and comembership2:
+			c += 1
+		elif not comembership1 and comembership2:
+			d ++=1	
+	M = a + b + c + d
+	m1 = a + b
+	m2 = a + c
+	numerator = (M*a) - (m1*m2)
+	denominator = sqrt(m1 * m2 * (M - m1 ) * (M - m2))
+    return numerator/denominator
+
+
+def Compute_matthews_corrcoef(labels_true, labels_pred) :
+	"""matthews_corrcoef (SKLEARN)
+	Reference:
+	http://scikit-learn.org/stable/modules/generated/sklearn.metrics.matthews_corrcoef.html
+	"""
+	return metrics.matthews_corrcoef(labels_true, labels_pred)
 
 
 def Compute_silhouette_score(data,labels_pred):
